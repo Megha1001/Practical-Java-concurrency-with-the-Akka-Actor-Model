60a91d66b195926d84da2f2902badf69e221bef9
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2023-05-07 14:02:41 +0530

    Update MultiThreadedBigPrimes.java

--- JavaBigPrimesSimple/MultiThreadedBigPrimes.java
+++ JavaBigPrimesSimple/MultiThreadedBigPrimes.java
@@ -5,9 +5,14 @@ public class MultiThreadedBigPrimes {
     public static <List> void main(String[] args) throws InterruptedException {
         Long start = System.currentTimeMillis();
         Results results = new Results();
+
+        Runnable status = new CurrentStatus(results);
+        Thread statusTask = new Thread(status);
+        statusTask.start();
+
         Runnable task = new PrimeGenerator(results);
         java.util.List<Thread> threads = new ArrayList<>();
-        for(int i =0; i < 20; i++){
+        for(int i =0; i < 100; i++){
             Thread t = new Thread(task);
             threads.add(t);
             t.start();
@@ -15,7 +20,7 @@ public class MultiThreadedBigPrimes {
         for(Thread t : threads){
             t.join();
         }
-        System.out.println(results.getPrimes());
+        results.print();
         Long end = System.currentTimeMillis();
         System.out.println("Time Taken :"+(end - start)+" ms.");
     }
