c276e56cf163a199e02416d3e43bfac6fed3d66a
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2023-05-07 12:55:56 +0530

    Create PrimeGenerator.java

---
+++ JavaBigPrimesSimple/PrimeGenerator.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class PrimeGenerator implements Runnable{
+
+    private Results results;
+
+    public PrimeGenerator(Results results){
+        this.results = results;
+    }
+    @Override
+    public void run() {
+        // Creating random number from 0 to 2^2000 - 1
+        BigInteger bigInteger = new BigInteger(2000, new Random());
+        results.getPrimes().add(bigInteger.nextProbablePrime());
+    }
+}
